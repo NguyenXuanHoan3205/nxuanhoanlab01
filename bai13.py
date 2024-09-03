814a3cbba983e4e709a4a7ab674c19163c8f4028
NguyenXuanHoan3205 <honanguyen2005@gmail.com>
2024-09-03 21:22:04 +0700

    bai13.py

--- bai13.py
+++ bai13.py
@@ -5,13 +5,12 @@ Created on Mon Sep 2 18:55:04 2024
 @author: Nguyễn Xuân Hoan - 23694771
 """
 
-def format_date(day, month, year):
-    print(f"a. {day}/{month}/{year}")
-    print(f"b. {day}/{month}/{year:02d}")
-    print(f"c. {year}/{month}/{day}")
-day=int(input("Nhap vao ngay sinh: "))
-month=int(input("Nhap vao thang sinh: "))
-year=int(input("Nhap vao nam sinh: "))
-format_date(day, month, year)
-
+day = int(input("Nhập ngày: "))
+month = int(input("Nhập tháng: "))
+year = int(input("Nhập năm: "))
+d = year%100
+print("a> Ngày/tháng/năm: ",f"{day}/{month}/{year}")
+print("b> Ngày/tháng/năm: ",f"{day}/{month}/{d}")
+print("c> Năm/tháng/ngày: ",f"{year}/{month}/{day}")
+    
     
